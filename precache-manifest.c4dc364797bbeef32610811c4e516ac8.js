64278014213ac8d9780f0264d81da16277d52da9
Hau Chong <thchong-code@users.noreply.github.com>
2020-09-10 02:52:38 +0800

    Updates

---
+++ precache-manifest.c4dc364797bbeef32610811c4e516ac8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cb46aec3184b65fea89ffb6aa839fa",
+    "url": "/rainorshine/index.html"
+  },
+  {
+    "revision": "02a13badf4023d4af432",
+    "url": "/rainorshine/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b23c39328d92825b6f00",
+    "url": "/rainorshine/static/css/main.f4aa1536.chunk.css"
+  },
+  {
+    "revision": "02a13badf4023d4af432",
+    "url": "/rainorshine/static/js/2.ab2a144b.chunk.js"
+  },
+  {
+    "revision": "17dffa516c2ca25cecdb67ecbc5e1c6e",
+    "url": "/rainorshine/static/js/2.ab2a144b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23c39328d92825b6f00",
+    "url": "/rainorshine/static/js/main.7cdb0e9a.chunk.js"
+  },
+  {
+    "revision": "ad036da2879367e7e995",
+    "url": "/rainorshine/static/js/runtime-main.9410802d.js"
+  }
+]);
