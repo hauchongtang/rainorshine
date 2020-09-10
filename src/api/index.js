4db1aa0aa2b4ba07a911679fd4b701c614ae3d7d
Hau Chong <thchong-code@users.noreply.github.com>
2020-09-10 17:27:22 +0800

    added 24 hr

--- src/api/index.js
+++ src/api/index.js
@@ -12,4 +12,14 @@ export const fetchData = async () => {
   } catch (error) {
     console.log(error);
   }
-}
+};
+
+export const fetch24Data = async () => {
+  try {
+    const url = 'https://api.data.gov.sg/v1/environment/24-hour-weather-forecast';
+    const response = await axios.get(url);
+    return (response.data.items[0].periods.map(item => item.regions));
+  } catch (error) {
+    console.log(error);
+  }
+};
