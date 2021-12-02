d449cae77c83fe5e4fb5e2aa6d33c97096eebdf5
hauchongtang <56034480+hauchongtang@users.noreply.github.com>
2021-12-02 20:55:13 +0800

    dark-purple theme

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -158,7 +158,8 @@ const Maps = () => {
       <div id="map">
       <Map center={current} zoom={12}>
         <TileLayer
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
+          //url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         {
